87e8f35f6a4de34cdda27fe6f7152c4d29f5faa5
Kerlos Alfy <64893108+kerlos-alfy@users.noreply.github.com>
2026-08-23 03:21:48 +0400

    Alpha 5: restore clean PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,12 +5,8 @@
  *
  * @package OxyAI
  */
+
 declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-if (!defined('DAY_IN_SECONDS')) {
-    define('DAY_IN_SECONDS', 86400);
-}
-
 require_once __DIR__ . '/stubs/wp-core-stubs.php';
